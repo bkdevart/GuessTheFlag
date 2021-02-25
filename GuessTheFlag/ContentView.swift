6df59b953a5840d8832d4d2bf1a3b6145d0a1950
Brandon Knox <bknox.dev@gmail.com>
2021-02-24 20:00:44 -0700

    project start state

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,12 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingAlert = false
     var body: some View {
-        Button("Show Alert") {
-            self.showingAlert = true
-        }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("Hello SwiftUI"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
-        }
+        Text("Hello World")
     }
 }
 
